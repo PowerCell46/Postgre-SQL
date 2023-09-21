00e356c275ab2b0b05bc0ae2a92c909de7dd2734
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-09-21 23:07:05 +0300

    Create New Table.sql

---
+++ Basic CRUD Exercise/New Table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS company_chart AS SELECT concat_ws(' ', first_name, last_name) as "Full Name", job_title as "Job Title", department_id as "Department ID", manager_id as "Manager ID" FROM employees;
