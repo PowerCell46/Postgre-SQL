40978644da1bd6d2ae8fddae21caab6236cdccd6
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-06 23:38:06 +0300

    Create Age Group.sql

---
+++ Data Aggregation Exercise/Age Group.sql
@@ -0,0 +1,12 @@
+SELECT CASE
+	WHEN age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN age BETWEEN 51 AND 60 THEN '[51-60]'
+	ELSE '[61+]'
+	END as age_group,
+	COUNT(id) AS count
+FROM wizard_deposits
+GROUP BY age_group
+ORDER BY age_group ASC;
