9dc82f873cdf8b0448cb8fe29f0130723615d348
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-08 23:28:13 +0300

    Create PRIMARY KEY.sql

---
+++ Table Relations Exercise/PRIMARY KEY.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS products (
+	product_name VARCHAR(100)
+);
+
+INSERT INTO products (product_name)
+VALUES ('Broccoli'),
+		('Shampoo'),
+		('Toothpaste'),
+		('Candy');
+		
+ALTER TABLE products
+ADD COLUMN id SERIAL PRIMARY KEY;
