b263763384662c816cc0164953d9e744717e92d4
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-08 23:27:32 +0300

    Create Delete Mountains.sql

---
+++ Table Relations Lab/Delete Mountains.sql
@@ -0,0 +1,14 @@
+CREATE TABLE mountains(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE peaks(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	mountain_id INTEGER,
+	CONSTRAINT fk_mountain_id
+		FOREIGN KEY (mountain_id)
+			REFERENCES mountains(id)
+				ON DELETE CASCADE
+);
