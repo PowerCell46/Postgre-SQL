2bda8facfb97d7f88963d6be94fd7ffe4731d4a9
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-14 11:18:03 +0300

    Create Insert.sql

---
+++ Exam/Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO coaches (first_name, last_name, salary, coach_level)
+SELECT
+	first_name, 
+	last_name, 
+	salary * 2,
+	LENGTH(first_name) AS coach_level
+FROM players 
+WHERE hire_date < '2013-12-13 07:18:46'
