cd090612573d92739917a5735dff2eb741d96a44
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-15 23:39:49 +0300

    Create Money Transfer.sql

---
+++ Functions and Procedures Exercise/Money Transfer.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE sp_transfer_money(sender_id INTEGER, receiver_id INTEGER, amount NUMERIC)
+AS 
+$$
+	DECLARE 
+		current_balance NUMERIC;
+	BEGIN
+		CALL sp_withdraw_money(sender_id, amount);
+		CALL sp_deposit_money(receiver_id, amount);
+	
+		SELECT balance INTO current_balance FROM accounts WHERE id = sender_id;
+		
+		IF (current_balance < 0) THEN
+			ROLLBACK;
+		END IF;
+		
+	END; 
+$$
+LANGUAGE plpgsql;
