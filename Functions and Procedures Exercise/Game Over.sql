492e28f2afbfea50274efff8adad8f49d9d9ed2b
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-15 23:38:18 +0300

    Create Game Over.sql

---
+++ Functions and Procedures Exercise/Game Over.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION fn_is_game_over(is_game_over BOOLEAN)
+RETURNS TABLE("name" VARCHAR(50), game_type_id INTEGER, is_finished BOOLEAN) AS
+$$
+	BEGIN
+		RETURN QUERY
+		SELECT 
+			games.name,
+			games.game_type_id, 
+			games.is_finished
+		FROM games
+		WHERE games.is_finished = is_game_over;
+	END
+$$
+LANGUAGE plpgsql;
